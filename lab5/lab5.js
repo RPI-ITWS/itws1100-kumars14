63fd8a80fd3254dbc4952ba2d5bd9173c9ae2039
kumars14rpi <kumars14@rpi.edu>
2022-11-30 22:11:05 -0500

    updated lab 5 and added to projects page

--- lab5/lab5.js
+++ lab5/lab5.js
@@ -5,10 +5,66 @@ function validate(formObj) {
    // put your validation code here
    // it will be a series of if statements
 
-   if (formObj.firstName.value == "") {
-      alert("You must enter a first name");
-      formObj.firstName.focus();
-      return false;
+   var alertText = ""
+   var focusSet = 0
+
+   if (formObj.firstName.value === "") {
+      alertText += "You must enter a first name\n";
+      if (focusSet === 0) {
+         formObj.firstName.focus();
+         focusSet = 1;
+      }
+   }
+
+   if (formObj.lastName.value === "") {
+      alertText += "You must enter a last name\n";
+      if (focusSet === 0) {
+         formObj.lastName.focus();
+         focusSet = 1;
+      }
+   }
+
+   if (formObj.title.value === "") {
+      alertText += "You must enter a title\n";
+      if (focusSet === 0) {
+         formObj.title.focus();
+         focusSet = 1;
+      }
+   }
+
+   if (formObj.forg.value === "") {
+      alertText += "You must enter an organization\n";
+      if (focusSet === 0) {
+         formObj.org.focus();
+         focusSet = 1;
+      }
+   }
+
+   if (formObj.pseudonym.value === "") {
+      alertText += "You must enter a nickname\n";
+      if (focusSet === 0) {
+         formObj.pseudonym.focus();
+         focusSet = 1;
+      }
+   }
+
+   if (formObj.comments.value === "" || formObj.comments.value == "Please enter your comments") {
+      alertText += "You must enter your comments\n";
+      if (focusSet === 0) {
+         formObj.comments.focus();
+         focusSet = 1;
+      }
+   }
+
+   if (alertText === "") {
+      alert("Form successfully submitted!");
+      return true;
+   }
+
+   if (alertText !== "") {
+      alert(alertText);
+      return (false);
    }
    return true;
 }
+
