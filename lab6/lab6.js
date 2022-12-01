9772f3c6a82e89ed948d827d04ae108253436c62
kumars14rpi <kumars14@rpi.edu>
2022-12-01 02:08:39 -0500

    updated lab6, added lab6 to projects page

--- lab6/lab6.js
+++ lab6/lab6.js
@@ -14,38 +14,56 @@ $(document).ready(function () {
       alert('You\'ve clicked the lab button');
    });
 
-
-
-
-
    // Problem 1: When the user clicks on the <h1>,
    //change the 'your name' to your own name (ie Joe Smith)
    //change the text to be your name in small caps
    //change the color to be something other than blue or black
    //change the text size to 110% or normal
    // (note that there is already a class defined for the area where your name should go)
-
-
-
+   $("h1").click(function () {
+      $(".myName").text("Shreya Kumar");
+      $(".myName").css("color", "purple");
+      $(".myName").css("text-transform", "lowercase");
+   })
 
    // Problem 2: Make the "lorem ipsum" paragraphs
    //   vanish over a 6/10 sec duration when a user clicks "Hide text";
    //   make it appear within a 1 1/2 second duration when a user clicks "Show text":
+   $("#hideText").click(function () {
+      setTimeout(function () {
+         $("p").fadeOut(0);
+      },600);
+   })
 
+   $("#showText").click(function () {
+      setTimeout(function () {
+         $("p").fadeIn(0);
+      },1000);
+   })
 
    // Problem 3: When a normal list item is clicked, make it turn red using addClass.
    //            When a red list item is clicked change it back (you need to look up the appropriate jQuery method to do this)
    // (Note that there already is a css style named ".red" in lab6.css)
-
+   $("li").click(function () {
+      if ($(this).hasClass("red")) {
+         $(this).removeClass("red");
+      }
+      else {
+         $(this).addClass("red");
+      }
+   });
 
    // Problem 4: When a user clicks on the "Add a list item" button, add a new list item to the end of the list.
    // Problem 4b - what happens when you click on the new li?  Why? (Explain in your readme file)
-
-
+   $("#AddListItem").click(function () {
+      $("#labList").append("<li>new list item</li>");
+   });
 
    // Problem 5: lookup another jquery method and use this code on the "Toggle Text"
    // link to show/hide the text:
-
+   $("#toggleText").click(function () {
+      $("p:not(:first)").toggle();
+   })
 
    /* When you are done:
      Add this to your github repos,
