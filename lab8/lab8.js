d404ab08c78c67bfcd93039a1837e1f854eda3fc
kumars14rpi <kumars14@rpi.edu>
2022-12-05 04:05:13 -0500

    edits to lab8

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
-    var option=1;	
-	
-	if (option==1) {
-	
-  	$.ajax({
-        type: "GET",
-   	 	url: "lab8.js",
-   	 	dataType: "json",
-    })
-}
+	$.getJSON("lab8.json",
+		function(data) {
+			var result = '';
+			$.each(data, function (key, value) {
+				output += '<ul>';
+				output += '<li> <a href="' + value.link + value.link2 + '">' + value.number + ' - ' + value.name+'</a></li>';
+				output += '<li>' + value.description + '</li>';
+				output += '</ul>';
+			});
+			$('#laboutput').append(output);
+		});	
+});
 
+$(function () {
+	$('.heading1').animate({height: '60px' }, 1000, 'linear');
 });
