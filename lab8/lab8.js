0cb57370e0b5d90fbbfa929ae517f145efe45a55
kumars14rpi <kumars14@rpi.edu>
2022-12-08 18:29:26 -0500

    edits to lab8

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -1,17 +1,22 @@
-$(document).ready(function() {
-	$.getJSON("lab8.json",
-		function(data) {
-			var result = '';
-			$.each(data, function (key, labs) {
-				output += '<ul>';
-				output += '<li> <a href="' + labs.link + labs.link2 + '">' + labs.number + ' - ' + labs.name+'</a></li>';
-				output += '<li>' + labs.description + '</li>';
-				output += '</ul>';
-			});
-			$('#laboutput').append(output);
-		});	
+$(document).ready(function () {
+	$.getJSON("lab8.json", 
+	// jQuery function to get the JSON file
+		function (data) {
+		var output = '';
+		// variable for an empty string 
+		$.each(data, function (key, value) {
+		// jQuery function to run for every input of the JSON file
+			output += '<ul class="items">';
+			output += '<a href="' + value.link + '">' + value.number + ' with ' + value.name+ '</a>';
+			output += '<li>' + value.major + '</li>';
+            output += '<li>' + ' About the lab: ' + value.description + '</li>';
+			output += '</ul>';
+			// series of output concatenation to start with a list item, and add the lab number and title, which points to 
+			// its link. another list item for the description, and the closing list tag
+		});
+		$('#laboutput').append(output);
+		//jQuery to add the output to the div
+
+	});
 });
 
-$(function () {
-	$('.heading1').animate({height: '60px' }, 1000, 'linear');
-});
